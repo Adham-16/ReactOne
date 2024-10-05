8d0caebce18fac30aff81bd213cf3bd6eb217700
Adham-16 <a98347612z@gmail.com>
2024-10-06 01:59:02 +0300

    Performance improvements

--- src/Component/Contact/Contact.jsx
+++ src/Component/Contact/Contact.jsx
@@ -8,13 +8,23 @@ export function Contact() {
         password: ''
     });
 
-    const handleInputChange = (event) => {
+    const [showLabels, setShowLabels] = useState({
+        name: false,
+        age: false,
+        email: false,
+        password: false
+    });
+
+    function handleInputChange(event) {
         const { name, value } = event.target;
-        setFormData(prevData => ({ ...prevData, [name]: value }));
-    };
+        setFormData({ ...formData, [name]: value });
+        setShowLabels({ ...showLabels, [name]: value.trim() !== "" });
+    }
 
     const validateForm = () => {
-        return Object.values(formData).every(field => field.trim() !== "");
+        return (
+            formData.name && formData.age && formData.email && formData.password
+        );
     };
 
     const handleSubmit = (event) => {
@@ -23,56 +33,64 @@ export function Contact() {
             alert('Please fill in all fields!');
             return;
         }
+        // Sending data
     };
 
     return (
-        <div className="Home-app">
-            <h1 className="fw-bold mb-3 c-2C3E50 text-uppercase">Contact section</h1>
-            <div className="form w-100">
-                <form className="w-50 p-3 mx-auto mt-5" onSubmit={handleSubmit}>
-                    <label htmlFor="name" className="position-relative c-1abc9c">User Name:</label>
-                    <input 
-                        type="text" 
-                        value={formData.name} 
-                        onChange={handleInputChange} 
-                        name="name" 
-                        placeholder="User Name" 
-                        className="border-0 form-control my-4 border-bottom py-3 position-relative form" 
-                    />
+        <>
+            <div className="Home-app">
+                <h1 className="fw-bold mb-3 c-2C3E50 text-uppercase">Contact section</h1>
+                <div className="shape d-flex justify-content-center align-items-center">
+                    <div className="Right border-bottom-color"></div>
+                    <i className="fa-solid fa-star text-white mx-3 c-2C3E50"></i>
+                    <div className="Left border-bottom-color"></div>
+                </div>
+                <div className="form w-100">
+                    <form className="w-50 p-3 mx-auto mt-5" onSubmit={handleSubmit}>
+                        {showLabels.name && <label htmlFor="userName" className="position-relative c-1abc9c">userName:</label>}
+                        <input 
+                            type="text" 
+                            value={formData.name} 
+                            onChange={handleInputChange} 
+                            name="name" 
+                            placeholder="userName" 
+                            className="userName border-0 form-control my-4 border-bottom py-3 position-relative form" 
+                        />
 
-                    <label htmlFor="age" className="position-relative c-1abc9c">User Age:</label>
-                    <input 
-                        type="number" 
-                        value={formData.age} 
-                        onChange={handleInputChange} 
-                        name="age" 
-                        placeholder="User Age" 
-                        className="border-0 form-control my-4 border-bottom py-3 position-relative form" 
-                    />
+                        {showLabels.age && <label htmlFor="userAge" className="position-relative c-1abc9c">userAge:</label>}
+                        <input 
+                            type="number" 
+                            value={formData.age} 
+                            onChange={handleInputChange} 
+                            name="age" 
+                            placeholder="userAge" 
+                            className="userAge border-0 form-control my-4 border-bottom py-3 position-relative form" 
+                        />
 
-                    <label htmlFor="email" className="position-relative c-1abc9c">User Email:</label>
-                    <input 
-                        type="email" 
-                        value={formData.email} 
-                        onChange={handleInputChange} 
-                        name="email" 
-                        placeholder="User Email" 
-                        className="border-0 form-control my-4 border-bottom py-3 position-relative form" 
-                    />
+                        {showLabels.email && <label htmlFor="userEmail" className="position-relative c-1abc9c">userEmail:</label>}
+                        <input 
+                            type="email" 
+                            value={formData.email} 
+                            onChange={handleInputChange} 
+                            name="email" 
+                            placeholder="userEmail" 
+                            className="userEmail border-0 form-control my-4 border-bottom py-3 position-relative form" 
+                        />
 
-                    <label htmlFor="password" className="position-relative c-1abc9c">User Password:</label>
-                    <input 
-                        type="password" 
-                        value={formData.password} 
-                        onChange={handleInputChange} 
-                        name="password" 
-                        placeholder="User Password" 
-                        className="border-0 form-control my-4 border-bottom py-3 position-relative form" 
-                    />
+                        {showLabels.password && <label htmlFor="userPassword" className="position-relative c-1abc9c">userPassword:</label>}
+                        <input 
+                            type="password" 
+                            value={formData.password} 
+                            onChange={handleInputChange} 
+                            name="password" 
+                            placeholder="userPassword" 
+                            className="userPassword border-0 form-control my-4 border-bottom py-3 position-relative form" 
+                        />
 
-                    <button type="submit" className="btn mt-4 text-white bg-1abc9c" disabled={!validateForm()}>Send Message</button>
-                </form>
+                        <button type="submit" className="btn mt-4 text-white bg-1abc9c">send Message</button>
+                    </form>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
